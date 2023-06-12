2a4c9735248c23cb6ee62db716991e21a3c9de0d
baekeys <rizqiakbarmem12@gmail.com>
2023-06-12 19:51:05 +0800

    Membuat class MatematikaRun

--- src/hitung/MatematikaRun.java
+++ src/hitung/MatematikaRun.java
@@ -10,5 +10,16 @@ package hitung;
  * @author rizqi
  */
 public class MatematikaRun {
+    public static void main(String[] args) {
+        
+        //membuat objek
+        Matematika rizqi = new Matematika(8,2);
+        
+        System.out.println("Hasil Penjumlahan : "+rizqi.setPenjumlahan());
+        System.out.println("Hasil Pengurangan : "+rizqi.setPengurangan());
+        System.out.println("Hasil Perkalian : "+rizqi.setPerkalian());
+        System.out.println("Hasil Pembagian : "+rizqi.setPembagian());
+    }
+   
     
 }
